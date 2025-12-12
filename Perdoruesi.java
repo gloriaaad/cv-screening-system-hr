62cf381c4cfa403a1adcbe50c2be1989405d1823
gloriaaad <78726965+gloriaaad@users.noreply.github.com>
2025-12-12 19:29:02 +0100

    Update Perdoruesi.java

--- Perdoruesi.java
+++ Perdoruesi.java
@@ -21,5 +21,67 @@ public class Perdoruesi {
     @Enumerated(EnumType.STRING)
     private StatusPerdoruesi status;
 
-    // getters & setters
+    public Long getPerdoruesId() {
+        return perdoruesId;
+    }
+    
+    public void setPerdoruesId(Long perdoruesId) {
+        this.perdoruesId = perdoruesId;
+    }
+    
+    public String getEmer() {
+        return emer;
+    }
+    
+    public void setEmer(String emer) {
+        this.emer = emer;
+    }
+    
+    public String getMbiemer() {
+        return mbiemer;
+    }
+    
+    public void setMbiemer(String mbiemer) {
+        this.mbiemer = mbiemer;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getFjalekalim() {
+        return fjalekalim;
+    }
+    
+    public void setFjalekalim(String fjalekalim) {
+        this.fjalekalim = fjalekalim;
+    }
+    
+    public Role getRoli() {
+        return roli;
+    }
+    
+    public void setRoli(Role roli) {
+        this.roli = roli;
+    }
+    
+    public LocalDate getDataRegjistrimit() {
+        return dataRegjistrimit;
+    }
+    
+    public void setDataRegjistrimit(LocalDate dataRegjistrimit) {
+        this.dataRegjistrimit = dataRegjistrimit;
+    }
+    
+    public StatusPerdoruesi getStatus() {
+        return status;
+    }
+    
+    public void setStatus(StatusPerdoruesi status) {
+        this.status = status;
+    }
 }
